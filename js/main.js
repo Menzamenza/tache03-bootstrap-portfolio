b422c02e0ea7284cb740e0ed2c544c602c51c359
Menza <menzamenza90@gmail.com>
2023-10-25 23:23:31 +0000

    first part

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// header scrol
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    navCollapse.classList.remove("show")
+})
